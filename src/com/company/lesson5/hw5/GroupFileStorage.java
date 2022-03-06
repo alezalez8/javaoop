2ab6a36f75708713f7c8d819055b5e3fe256044f
alezalez8 <5446943@ukr.net>
2022-03-06 20:44:21 +0200

    hw5 task 2 method saveGroupToCSV work

--- src/com/company/lesson5/hw5/GroupFileStorage.java
+++ src/com/company/lesson5/hw5/GroupFileStorage.java
@@ -1,7 +1,9 @@
 package com.company.lesson5.hw5;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.util.Scanner;
 
 import com.company.lesson4.hw4.*;
@@ -13,11 +15,24 @@ public class GroupFileStorage {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите путь для сохранения файла " + group.getGroupName());
         String path = scanner.nextLine();
-        File file = new File(path + "\\" + group.getGroupName() + "\\.csv");
+        //File file = new File(path + "\\" + group.getGroupName() + "\\.csv");
+        String csvFileName = group.getGroupName() + ".csv";
+        File file = new File(path, csvFileName);
         file.createNewFile();
+        try (Writer writer = new FileWriter(file)) {
+            //  Student[] students = group.getStudents();
+            CSVStringConverter converter = new CSVStringConverter();
 
+            for (int i = 0; i < group.getStudents().length; i++) {
 
-
+                //  converter.toStringRepresentation(students[i]);
+                if ((group.getStudents())[i] != null) {
+                    String csvStroka = converter.toStringRepresentation((group.getStudents())[i]);
+                    writer.write(csvStroka);
+                }
+            }
+            System.out.println("Файл успешно сохранен");
+        }
 
     }
 
--- src/com/company/lesson5/hw5/Starter.java
+++ src/com/company/lesson5/hw5/Starter.java
@@ -1,5 +1,10 @@
 package com.company.lesson5.hw5;
 
+import com.company.lesson4.hw4.Gender;
+import com.company.lesson4.hw4.Group;
+import com.company.lesson4.hw4.GroupOverflowException;
+import com.company.lesson4.hw4.Student;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -7,12 +12,37 @@ public class Starter {
 
     public static void main(String[] args) throws IOException {
 
+        GroupFileStorage storage = new GroupFileStorage();
+        Group group = new Group("javaOOP");
+
+        Student student1 = new Student("Aleks", "Shunin", Gender.MALE);
+        Student student2 = new Student("Gena", "Ivanov", Gender.MALE);
+        Student student3 = new Student("Sasha", "Petrov", Gender.MALE);
+
+        try {
+            group.addStudent(student1);
+            group.addStudent(student2);
+            group.addStudent(student3);
+        } catch (GroupOverflowException e) {
+            System.out.println(e);
+        }
+
+        storage.saveGroupToCSV(group);
+
+
+
+
+/*
         File source = new File("C:\\Users\\Администратор\\Desktop\\A");
         File target = new File("C:\\Users\\Администратор\\Desktop\\M");
         String extens = "pdf";
 
         CopeFileService fileService = new CopeFileService(source, target, extens);
         fileService.copyDefiniteFile();
+*/
 
     }
 }
+
+//  C:\\Users\\Администратор\\Desktop\\M
+// C:\Users\Администратор\Desktop\M
