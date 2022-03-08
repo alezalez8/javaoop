74438773c0a169cae32e193ca35f1a4dcf9ea960
alezalez8 <5446943@ukr.net>
2022-03-08 20:45:44 +0200

    hw5 advance end

--- src/com/company/lesson5/hw5/FileCompareService.java
+++ src/com/company/lesson5/hw5/FileCompareService.java
@@ -16,16 +16,33 @@ public class FileCompareService {
 
 
     public boolean isFileEquals(File fileOne, File fileTwo) throws IOException {
+        long sizeOne = 0;
+        long sizeTwo = 0;
         byte[] arrayOne = new byte[1000];
         byte[] arrayTwo = new byte[1000];
         try (InputStream inputStreamOne = new FileInputStream(fileOne);
              InputStream inputStreamTwo = new FileInputStream(fileTwo)) {
-
-
+            for (; (inputStreamOne.available() > 0) && (inputStreamTwo.available() > 0); ) {
+                sizeOne += inputStreamOne.read(arrayOne);
+                sizeTwo += inputStreamTwo.read(arrayTwo);
+                if (!compareArrays(arrayOne, arrayTwo)) {
+                    return false;
+                }
+            }
         }
+        System.out.println("file one contains: " + sizeOne + " bytes and file two contains: " + sizeTwo);
+        return true;
+    }
 
-
-        return false;
+    private boolean compareArrays(byte[] arr1, byte[] arr2) {
+        if (arr1.length == arr2.length) {
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] != arr2[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 
--- src/com/company/lesson5/hw5/FileCompareTest.java
+++ src/com/company/lesson5/hw5/FileCompareTest.java
@@ -1,9 +1,36 @@
 package com.company.lesson5.hw5;
 
+import java.io.File;
+import java.io.IOException;
+
 public class FileCompareTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        FileCompareService compareService = new FileCompareService(args);
+        FileCompareService compareService = new FileCompareService();
+        File file1;
+        File file2;
+        if (args.length == 2) {
+            file1 = new File(args[0]);
+            file2 = new File(args[1]);
+            if (compareService.isFileEquals(file1, file2)) {
+                System.out.println("Файлы идеинтичны");
+            } else {
+                System.out.println("Файлы разные");
+            }
+
+        } else {
+
+            file1 = new File("C:\\Users\\Администратор\\Desktop\\M\\test2.txt");
+            file2 = new File("C:\\Users\\Администратор\\Desktop\\A\\test2.txt");
+
+            if (compareService.isFileEquals(file1, file2)) {
+                System.out.println("Файлы идеинтичны");
+            } else {
+                System.out.println("Файлы разные");
+            }
+
+        }
+
     }
 }
