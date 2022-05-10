13a8bebf19174c524bc3ecc600dc1f1db8c3231e
alezalez8 <5446943@ukr.net>
2022-05-10 14:23:35 +0300

    lesson class 7 rev 2

--- src/com/company/lesson7/classwork/Printer.java
+++ src/com/company/lesson7/classwork/Printer.java
@@ -1,22 +1,21 @@
 package com.company.lesson7.classwork;
 
 public class Printer {
-        private Long threadId = null;
 
-        public synchronized void printText(String text) {
-            Thread thr = Thread.currentThread();
-            long id = thr.getId();
+    private Long idThread = null;
 
-            for (; !(threadId == null || threadId != id); ) {
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    // TODO: handle exception
-                }
+
+    public synchronized void printText(String text) {
+        long id = Thread.currentThread().getId();
+        for (; !(idThread == null || idThread != id); ) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            System.out.println(text);
-            threadId = id;
-            notifyAll();
         }
-
+        System.out.println(text);
+        idThread = id;
+        notify();
     }
+}
