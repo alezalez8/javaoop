b2e604cc00d722ad247d4ca3099ee7c775e19654
alezalez <5446943a@gmail.com>
2022-05-11 20:57:17 +0300

    lesson 8 classroom end

--- src/com/company/lesson8/classwork/Cat.java
+++ src/com/company/lesson8/classwork/Cat.java
@@ -1,6 +1,8 @@
 package com.company.lesson8.classwork;
 
-public class Cat {
+import java.util.Objects;
+
+public class Cat implements Cloneable {
     private String name;
     private int age;
     public Cat(String name, int age) {
@@ -18,5 +20,26 @@ public class Cat {
         return age;
     } public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return    "Cat [name=" + name + ", age=" + age + "]";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cat other = (Cat) obj;
+        return age == other.age && Objects.equals(name, other.name);
+    }
+
+    @Override
+    protected Cat clone() throws CloneNotSupportedException {
+        return (Cat)super.clone();
     }
 }
--- src/com/company/lesson8/classwork/Main.java
+++ src/com/company/lesson8/classwork/Main.java
@@ -1,7 +1,57 @@
 package com.company.lesson8.classwork;
 
-public class Main {
-    public static void main(String[] args) {
+import java.lang.reflect.Field;
 
-    }
+public class Main {
+
+        public static void main(String[] args) {
+            Cat cat1 = new Cat("Vaska", 5);
+            Cat cat2 = new Cat("Vaska", 5);
+
+            System.out.println(cat1 == cat2);
+            System.out.println(cat1.equals(cat2));
+            System.out.println(cat1.hashCode());
+            System.out.println(cat2.hashCode());
+            System.out.println("-----------------------------");
+
+            try {
+                Cat cat3 = cat1.clone();
+
+                System.out.println(cat3 == cat1);
+                System.out.println(cat3.getClass().equals(cat1.getClass()));
+                System.out.println(cat3.equals(cat1));
+
+            } catch (CloneNotSupportedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            System.out.println("-----------------------------");
+
+            Class catClass = cat1.getClass();
+
+            Field[] catFields = catClass.getDeclaredFields();
+            for (int i = 0; i < catFields.length; i++) {
+                System.out.println(catFields[i]);
+            }
+            System.out.println("-----------------------------");
+
+            Class[] interfaces = catClass.getInterfaces();
+            for (int i = 0; i < interfaces.length; i++) {
+                System.out.println(interfaces[i]);
+            }
+            System.out.println("-----------------------------");
+
+            try {
+                Field catAge = catClass.getDeclaredField("age");
+                catAge.setAccessible(true);
+                catAge.setInt(cat1, 100500);
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            System.out.println(cat1);
+
+        }
 }
