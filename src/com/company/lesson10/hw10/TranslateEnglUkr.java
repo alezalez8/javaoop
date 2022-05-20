db04a22b15dd55e4287c42dd3077fb44cae8de18
alezalez8 <5446943@ukr.net>
2022-05-20 13:42:34 +0300

    hw10 begin

---
+++ src/com/company/lesson10/hw10/TranslateEnglUkr.java
@@ -0,0 +1,8 @@
+package com.company.lesson10.hw10;
+
+public class TranslateEnglUkr {
+    public static void main(String[] args) {
+
+
+    }
+}
--- src/com/company/lesson9/hw9/SimpleList.java
+++ src/com/company/lesson9/hw9/SimpleList.java
@@ -1,6 +1,5 @@
 package com.company.lesson9.hw9;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
