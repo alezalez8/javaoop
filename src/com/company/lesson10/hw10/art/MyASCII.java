30d2f9344b70c5de8f1e41dfccbd5a5072acb6af
alezalez8 <5446943@ukr.net>
2022-05-23 16:04:45 +0300

    add socket rev1

---
+++ src/com/company/lesson10/hw10/art/MyASCII.java
@@ -0,0 +1,47 @@
+package com.company.lesson10.hw10.art;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MyASCII {
+    private Map<String, Character> myAsc = new HashMap<>();
+    private List<String[]> myAscTwo = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+        private String[] a = { "   #   ", "  # #  ", " #   # ", "#     #", "#######","#     #","#     #"};
+        private String[] b = { "###### ", "#     #", "#     #", "###### ", "#     #","#     #","###### " };
+        private String[] c = { " ##### ", "#     #", "#      ", "#      ", "#      " , "#     #"," ##### "};
+        private String[] d = { "###### ", "#     #", "#     #", "#     #", "#     #" , "#     #","###### "};
+        private String[] e = { "#######", "#      ", "#      ", "#####  ", "#      ", "#      ","#######"};
+        private String[] f = { "#######", "#      ", "#      ", "#####  ", "#      " , "#      ","#      "};
+        private String[] g = { " ##### ", "#     #", "#      ", "#  ####", "#     #" , "#     #"," ##### "};
+        private String[] h = { "#     #", "#     #", "#     #", "#######", "#     #" , "#     #","#     #"};
+        private String[] i = { "###", " # ", " # ", " # ", " # " , " # ","###"};
+        private String[] j = { "      #", "      #", "      #", "      #", "#     #", "#     #"," ##### "};
+        private String[] k = { "#    #", "#   # ", "#  #  ", "###   ", "#  #  ", "#   # ","#    #" };
+        private String[] l = { "#      ", "#      ", "#      ", "#      ", "#      " , "#      ","#######"};
+        private String[] m = { "#     #", "##   ##", "# # # #", "#  #  #", "#     #" , "#     #","#     #"};
+        private String[] n = { "#     #", "##    #", "# #   #", "#  #  #", "#   # #", "#    ##","#     #"};
+        private String[] o = { " ##### ", "#     #", "#     #", "#     #", "#     #" , "#     #"," ##### "};
+        private String[] p = { "###### ", "#     #", "#     #", "###### ", "#      " , "#      ","#      "};
+        private String[] q = { " ##### ", "#     #", "#     #", "#     #", "#   # #" , "#    # "," #### #"};
+        private String[] r = { "###### ", "#     #", "#     #", "###### ", "#   #  " , "#    # ","#     #"};
+        private String[] s = { " ##### ", "#     #", "#      ", " ##### ", "      #", "#     #"," ##### "};
+        private String[] t = { "#######", "   #   ", "   #   ", "   #   ", "   #   ", "   #   ","   #   " };
+        private String[] u = { "#     #", "#     #", "#     #", "#     #", "#     #" , "#     #"," ##### "};
+        private String[] v = { "#     #", "#     #", "#     #", "#     #", " #   # " , "  # #  ","   #   "};
+        private String[] w = { "#     #", "#  #  #", "#  #  #", "#  #  #", "#  #  #", "#  #  #"," ## ## " };
+        private String[] x = { "#     #", " #   # ", "  # #  ", "   #   ", "  # #  " , " #   # ","#     #"};
+        private String[] y = { "#     #", " #   # ", "  # #  ", "   #   ", "   #   " , "   #   ","   #   "};
+        private String[] z = { "#######", "     # ", "    #  ", "   #   ", "  #    " , " #     ","#######"};
+
+
+
+}
